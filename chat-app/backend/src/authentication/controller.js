bfb7b0c64c49d73bd22c352968f7a82eef2c59b9
Guidomp11 <ponceguido@gmail.com>
2022-08-27 19:44:21 -0300

    change handle errors

--- chat-app/backend/src/authentication/controller.js
+++ chat-app/backend/src/authentication/controller.js
@@ -6,7 +6,7 @@ module.exports = {
         try {
             const { user } = req.body;
 
-            if(!user) throw new Error({ status: STATUS.BAD_REQUEST, message: "User information incomplete." });
+            if(!user) throw {status: STATUS.BAD_REQUEST, message: "User information incomplete." };
 
             const _user = await createUser(user);
 
@@ -16,6 +16,7 @@ module.exports = {
             });
         }catch(error) {
             const status = error.status || STATUS.SERVER_ERROR;
+
             return res.status(status).json({
                 success: false,
                 message: error.message
@@ -44,7 +45,7 @@ module.exports = {
         try {
             const { token } = req.headers["Authorization"];
 
-            if(!token) throw new Error({ status: STATUS.BAD_REQUEST, message: "Empty token." });
+            if(!token) throw { status: STATUS.BAD_REQUEST, message: "Empty token." };
 
             const _user = await authenticateUser(token);
 
