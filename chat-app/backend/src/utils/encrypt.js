b5791eaa0e179eae3efa4fcc66b0eec60aaf1aed
Guidomp11 <ponceguido@gmail.com>
2022-08-27 19:43:58 -0300

    fix encrypt methods

--- chat-app/backend/src/utils/encrypt.js
+++ chat-app/backend/src/utils/encrypt.js
@@ -1,12 +1,14 @@
 const crypto = require("crypto-js");
 const { SECRET } = process.env;
 
-const encrypt = async (_value) => crypto.AES.encrypt(_value, SECRET).toString();
+const SECRET_STRING = SECRET.toString();
+
+const encrypt = async (_value) => await crypto.AES.encrypt(_value, SECRET_STRING).toString();
 
 const decrypt = async (_encrypted_data) => {
     if(!_encrypted_data) return null;
     
-    const bytes = crypto.AES.decrypt(_encrypted_data, SECRET);
+    const bytes = crypto.AES.decrypt(_encrypted_data, SECRET_STRING);
     return bytes.toString(crypto.enc.Utf8);
 };
 
