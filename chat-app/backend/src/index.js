5e33592e3ee74569e56f28e5b9ef50b36b8d5436
Guidomp11 <ponceguido@gmail.com>
2022-08-27 19:44:50 -0300

    add app middlewares

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 
+//#region APP MIDDLEWARES
+const cors = require("cors");
+const helmet = require("helmet");
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json({limit: '5mb'}));
+app.use(express.urlencoded({limit: '5mb'}));
+//#endregion
+
 //#region AUTHENTICATION ROUTES
 const authRouter = require("./authentication/index.routes");
 app.use("/api/auth", authRouter);
