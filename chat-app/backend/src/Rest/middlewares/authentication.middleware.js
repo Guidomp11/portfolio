2c4a18184463da15a0b39c9589690ab0b636f8e6
Guidomp11 <ponceguido@gmail.com>
2022-09-02 17:50:56 -0300

    add JWT for authentication

---
+++ chat-app/backend/src/Rest/middlewares/authentication.middleware.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const { STATUS } = require("../constants");
+const { decrypt } = require("../utils/encrypt");
+const { JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+    try {
+        const _token_encrypted = req.header("Authorization");
+        const _token = await decrypt(token_encrypted);
+
+        if(!_token) throw new Error("Invalid Token");
+
+        const _encrypt = jwt.verify(_token, JWT_SECRET);
+        req.user = _encrypt.user;
+        
+        next();
+    }catch(error) {
+        return res.status(STATUS.UNAUTHORIZED).json({
+            success: false,
+            message: "Invalid Token"
+        });
+    }
+}
---
+++ chat-app/backend/src/Rest/utils/JWTGenerator.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const { STATUS } = require("../constants");
+const { encrypt } = require("./encrypt");
+const { JWT_SECRET } = process.env;
+
+module.exports = async (payload, onSuccess, onFailed) => {
+    jwt.sign(payload, JWT_SECRET, {
+        expiresIn: "30d"
+    }, async (error, token) => {        
+        if(error) onFailed({ status: STATUS.SERVER_ERROR, message: "Cannot generate JWT" });
+
+        const _encrypted_token = await encrypt(token);
+        onSuccess(_encrypted_token);
+    });
+}
